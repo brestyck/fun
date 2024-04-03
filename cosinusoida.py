b1abcfa9213e7287566134c834625dd814329e1a
шваз (невич) <47777597+brestyck@users.noreply.github.com>
2024-04-03 11:21:54 +0300

    plotter universal

--- cosinusoida.py
+++ cosinusoida.py
@@ -1,11 +1,12 @@
 from time import sleep
-from math import sin, cos
+from math import sin, cos, tan
 from os import system
-colors = ["\033[95m", "\033[36m", "\033[91m", "\033[92m", "\033[94m", "\033[45m"]
+colors = ["\033[95m", "\033[97m", "\033[91m", "\033[92m", "\033[94m", "\033[45m"]
 p = 0
 c = 0
 SIZE = 60
 HORIZSIZE = 250
+COLCODESDIV = len(colors) - 1
 screen = []
 for _ in range(HORIZSIZE):
     screen.append([" "]*SIZE)
@@ -24,18 +25,21 @@ def printscreen():
         print(string)
         
 
-AMPLIF = 1
-COLCODESDIV = len(colors) - 1
+AMPLIF = 8
+
 while True:
     reset_scr()
     print(colors[c%COLCODESDIV], end="")
     for x in range(1, HORIZSIZE):
-        innersin = ((sin( (x/10) + (p/10) )) + 1) / (2*AMPLIF)
-        # print(innersin)
-        y = int(innersin*SIZE)
+        zinnersin = tan( (x/AMPLIF) + (p/10) )
+        innersin = (zinnersin + 1) / (2)
+        print(innersin)
+        y = int((innersin)*SIZE)
+        print(y)
         xj = HORIZSIZE - x-1
         yj = SIZE - y-1
-        screen[xj][yj] = "*"
+        if xj >= 0 and xj < HORIZSIZE and yj >= 0 and yj < SIZE:
+            screen[xj][yj] = "*"
     printscreen()
     if p % 100 == 0: c+=1
     p += 1
