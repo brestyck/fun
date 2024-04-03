6bf2ebf6956dbc76be273c2330802124cf36b70f
brestyck <borissmazz@gmail.com>
2024-04-03 14:30:19 +0300

    Unigraph added

--- unigraph.py
+++ unigraph.py
@@ -6,8 +6,8 @@ colors = ["\033[95m", "\033[97m", "\033[91m", "\033[92m", "\033[94m", "\033[45m"
 p = 0
 c = 0
 SYST = platform.system() == "Windows"
-SIZE = 30
-HORIZSIZE = 130
+SIZE = 40
+HORIZSIZE = 150
 COLCODESDIV = len(colors) - 1
 screen = []
 for _ in range(HORIZSIZE):
@@ -25,22 +25,26 @@ def printscreen():
         for j in range(HORIZSIZE):
             string += screen[j][i]
         print(string)
-    if SYST: sleep(0.1);system("cls")
+    if SYST: sleep(0.2);system("cls")
     else: sleep(0.03); system("clear")
 
 AMPLIF = 14
+TA = 50
 
 while True:
     reset_scr()
     print(colors[c%COLCODESDIV], end="")
-    for x in range(1, HORIZSIZE):
-        zinnersin = sin(p/10*cos( (x/AMPLIF) + (p/100) ))
-        innersin = (zinnersin + 1) / (2)
-        y = int((innersin)*SIZE)
-        xj = HORIZSIZE - x-1
-        yj = SIZE - y-1
-        if xj >= 0 and xj < HORIZSIZE and yj >= 0 and yj < SIZE:
-            screen[xj][yj] = "*"
+    for x in range(0, HORIZSIZE):
+        for y in range(0, SIZE):
+            f = ((x-60-int(15*cos(p/TA)))**2 + (y-15-int(15*cos(p/TA)))**2 <= 100)
+            g = int(tan(x+p/TA)*abs(cos(x))*sin(p/TA)) == y
+            h = abs(x-(p%100)) + abs(y-5) <= 36
+            if h:
+                screen[HORIZSIZE-x-1][SIZE-y-1] = colors[4]+"#"+colors[1]
+            if f:
+                screen[HORIZSIZE-x-1][SIZE-y-1] = colors[0]+"*"+colors[1]
+            if g:
+                screen[HORIZSIZE-x-1][SIZE-y-1] = colors[3]+"0"+colors[1]
     printscreen()
     if p % 100 == 0: c+=1
     p += 1
